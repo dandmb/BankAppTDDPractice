cf8922bc1a254680a941babe5a60c2d7fecf3bc1
dandmb <danbizwa@gmail.com>
2026-03-30 22:36:59 +0200

    dao added

--- app/src/main/java/com/dmb25/bankapp/data/local/entity/TransactionEntity.kt
+++ app/src/main/java/com/dmb25/bankapp/data/local/entity/TransactionEntity.kt
@@ -1,3 +1,16 @@
 package com.dmb25.bankapp.data.local.entity
 
-data class TransactionEntity()
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "transactions")
+data class TransactionEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+    val amount: Double,
+    val description: String,
+    val type: String,
+    val categoryId: Long,
+    val date: String,
+    val accountId: Long
+)
